cd67ed3856c3706830aef1cb7dd8e1c2843259c8
Jessica Ferreira <jessiberteloni@gmail.com>
2022-11-10 17:29:17 -0300

    Introdução às variáveis em Python

--- aula07.py
+++ aula07.py
@@ -4,3 +4,16 @@
 # O sinal de = é o operador de atribuição. Ele é usado para 
 # atribuir um valor a uma nome (variável).
 # Uso: nome_variavel = expressão
+
+nome_completo = 'Jessica Ferreira'
+soma_dois_mais_dois = 2 + 2
+int_um = bool('1')
+print(int_um, type(int_um))
+print(nome_completo, soma_dois_mais_dois)
+print()
+
+nome = 'Jessica'
+idade = 26
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
