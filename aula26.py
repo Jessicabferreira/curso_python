f26140c6ba8277fdf48591823167ab33fc233cdf
Jessica Ferreira <jessiberteloni@gmail.com>
2022-11-25 15:24:01 -0300

    Formatação de strings com f-strings

---
+++ aula26.py
@@ -0,0 +1,15 @@
+"""
+Formatação básica de strings
+s - string
+d - int
+f - float
+.<número de digitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+Sinal + ou -
+Exemplo.: 0>-100,.1f
+Conversion flags - !r !s !a
+"""
