cc869cec0943f7b0662dfd9fa7727ccff31fead0
Jessica Ferreira <jessiberteloni@gmail.com>
2022-11-26 15:08:17 -0300

    Fatiamento de strings e a função len

--- aula27.py
+++ aula27.py
@@ -14,3 +14,4 @@ print(variavel[3])  # o espaço é um caracter válido
 print(variavel[-8:-2])
 print(len(variavel))
 print(variavel[0:9:2]) # passo ele pula o caracter
+print(variavel[::-1]) # vai inverter a string
