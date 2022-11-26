6af3e80138af3c3d55caa6752d149f2e94d5c8cf
Jessica Ferreira <jessiberteloni@gmail.com>
2022-11-26 15:00:43 -0300

    Fatiamento de strings e a função len

--- aula27.py
+++ aula27.py
@@ -8,3 +8,8 @@ obs.: a função len retorna a qtd
 de caracteres da str
 """
 variavel = 'Olá mundo'
+print(variavel[4:])
+print(variavel[:5])
+print(variavel[3])  # o espaço é um caracter válido
+print(variavel[-8:-2])
+print(len(variavel))
