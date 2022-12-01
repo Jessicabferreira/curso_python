5980b97ab97d33197ecc5535a14ab2de9dc9c544
Jessica Ferreira <jessiberteloni@gmail.com>
2022-12-01 19:04:25 -0300

    Variáveis, constantes e complexidade

--- aula30.py
+++ aula30.py
@@ -16,10 +16,10 @@ carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and \
 carro_multado_radar_1 = carro_passou_radar_1 and vel_carro_pass_radar_1
 
 if vel_carro_pass_radar_1:
-    print('Velocidade carro passou do radar 1')
+    print('Velocidade do carro passou do radar 1')
 
 if carro_passou_radar_1:
-    print('Carro passou radar 1')
+    print('Carro passou do radar 1')
 
 if carro_multado_radar_1:
-    print('carro multado em radar 1')
+    print('carro foi multado em radar 1')
