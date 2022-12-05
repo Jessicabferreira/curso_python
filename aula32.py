1a216074767467f884448219b4a43389a0076a91
Jessica Ferreira <jessiberteloni@gmail.com>
2022-12-05 15:00:19 -0300

    Exercicios

--- aula32.py
+++ aula32.py
@@ -3,3 +3,8 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informar que não é um número inteiro.
 """
+"""
+Faça um programa que pergunte a hora ao usúario e, baseando-se no horário
+descrito, exiba a saudação apropriada. Ex.
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
