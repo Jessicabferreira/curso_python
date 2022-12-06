1e34a42bf119e409651599e2e5b5543281fd3e37
Jessica Ferreira <jessiberteloni@gmail.com>
2022-12-06 17:04:19 -0300

    Exercicios

--- aula32.py
+++ aula32.py
@@ -3,8 +3,17 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informar que não é um número inteiro.
 """
+
+
 """
 Faça um programa que pergunte a hora ao usúario e, baseando-se no horário
 descrito, exiba a saudação apropriada. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+
+
+""" 
+Faça um programa que peça o primeiro nome do usúario. Se o nome tiver 44 letras ou
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva
+"Seu nome é normal"; maior que 6 "Seu nome é muito grande".
 """
