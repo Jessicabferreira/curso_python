bc5eec7d4cf29249db6454f8acb97034a7f94959
Jessicabferreira <jessiberteloni@gmail.com>
2023-07-18 14:50:14 -0300

    Tipo list - Introdução ás listas mutáveis

--- aula48.py
+++ aula48.py
@@ -1,1 +1,7 @@
-"""Tipo list - Introdução ás listas mutáveis"""
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários velores de qualquer tipo
+Conhecimentos reutilizáveis - indices e fatiamneto
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
