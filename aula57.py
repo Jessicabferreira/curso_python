c275f306334a2a594b8d0a7e8f8e717eedf7320e
Jessicabferreira <jessiberteloni@gmail.com>
2023-07-28 18:34:32 -0300

    enumerate para enumerar valores de iteráveis (pegar índices)

--- aula57.py
+++ aula57.py
@@ -1,3 +1,20 @@
 """ 
 enumerate - enumere iteráveis (índices)
 """
+
+# [(0, 'Maria'), (1, 'Helena'), (2, 'Luiz'), (3, 'João')]
+lista = ['Maria', 'Helena', 'Jessica']
+lista.append('Pedro')
+
+for indice, nome in enumerate(lista):
+    print(indice, nome, lista[indice])
+
+# for item in enumerate(lista):
+#     indice, nome = item
+#     print(indice, nome)
+
+
+# for tupla_enumerada in enumerate(lista):
+#     print('FOR da tupla:')
+#     for valor in tupla_enumerada:
+#         print(f'\t{valor}')
