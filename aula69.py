723fc6548b7c44f5d2cde9fdc3e04af4de6360a4
Jessicabferreira <jessiberteloni@gmail.com>
2023-09-20 18:33:58 -0300

    Introdução as funções def

--- aula69.py
+++ aula69.py
@@ -3,8 +3,8 @@ Introdução as funções (def) em Python
 Funções são trechos de código usados para 
 replicar determinada ação ao longo do seu código.
 Elas podem receber valores para parâmetros (argumentos)
-e retornar um vlor específico.
-Por padrão, dunções Python retornam None (nada).
+e retornar um valor específico.
+Por padrão, funções Python retornam None (nada).
 """
 
 # def Print(a, b, c):
